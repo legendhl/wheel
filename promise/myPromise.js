c55c20a1e93bf3a6c2b81dc718ee045d4eda6e3d
legendhl <legendhl@gmail.com>
2018-09-17 21:02:21 +0800

    promise.then return another promise

--- promise/myPromise.js
+++ promise/myPromise.js
@@ -29,20 +29,21 @@ class MyPromise {
     }
 
     then(resolveFunc, rejectFunc) {
-        resolveFunc = typeof resolveFunc === 'function' ? resolveFunc : function() {};
-        rejectFunc = typeof rejectFunc === 'function' ? rejectFunc : function() {};
-        if (this.status === FULFILLED) {
-            setTimeout(() => {
-                resolveFunc(this.value);
-            }, 0);
-        } else if (this.status === REJECTED) {
-            setTimeout(() => {
-                rejectFunc(this.error);
-            }, 0);
-        } else {
-            this.resolveFunc = resolveFunc;
-            this.rejectFunc = rejectFunc;
-        }
+        resolveFunc = typeof resolveFunc === 'function' ? resolveFunc : function () { };
+        rejectFunc = typeof rejectFunc === 'function' ? rejectFunc : function () { };
+        return new MyPromise((resolve, reject) => {
+            this.resolveFunc = ((...args) => { let res = resolveFunc.apply(this, args); resolve(res); }).bind(this);
+            this.rejectFunc = ((...args) => { let res = rejectFunc.apply(this, args); reject(res); }).bind(this);
+            if (this.status === FULFILLED) {
+                setTimeout(() => {
+                    this.resolveFunc(this.value);
+                }, 0);
+            } else if (this.status === REJECTED) {
+                setTimeout(() => {
+                    this.rejectFunc(this.error);
+                }, 0);
+            }
+        });
     }
 
     catch(rejectFunc) {
@@ -72,5 +73,6 @@ class MyPromise {
 
 let p1 = new MyPromise((resolve, reject) => { setTimeout(() => resolve('p1'), 2000) });
 let p2 = new MyPromise((resolve, reject) => { resolve('p2') });
-p1.then(val => console.log(`p1 val is ${val}`));
-p2.then(val => console.log(`p2 val is ${val}`));
+let p3 = p1.then(val => { console.log(`p1 val is ${val}`); return 123; });
+p2.then(val => { console.log(`p2 val is ${val}`); return 456; }).then(val => console.log(`another p2 val is ${val}`));
+p3.then(val => console.log(`p3 val is ${val}`));
